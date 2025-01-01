8a94c4d8cf22d59eb248c896ddc018956fd1cff7
SebastianPerez11 <sebassperez11@gmail.com>
2025-01-01 19:29:17 -0300

    configure archive tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,9 +5,9 @@ export default {
 		extend: {},
 		gridTemplateColumns: {
 			'auto-services': 'repeat(auto-fit, minmax(450px, 1fr))',
-			'auto-services-mb': 'repeat(auto-fit, minmax(300px, 1fr))',
+			'auto-services-mb': 'repeat(auto-fit, minmax(280px, 1fr))',
 			'auto-data': 'repeat(auto-fit, minmax(400px, 1fr))',
-			'auto-data-mb': 'repeat(auto-fit, minmax(300px, 1fr))',
+			'auto-data-mb': 'repeat(auto-fit, minmax(280px, 1fr))',
 			'auto-12': 'repeat(12, minmax(0, 1fr))',
 		
 		},
