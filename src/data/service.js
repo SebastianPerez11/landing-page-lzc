8738d80705300ec3966c491c2754c5c45ecb74f2
SebastianPerez11 <sebassperez11@gmail.com>
2025-01-13 13:58:52 -0300

    add pages dynamics

---
+++ src/data/service.js
@@ -0,0 +1,32 @@
+export const services = [
+  {
+    id: "Albañilería",
+    title: "Albañilería",
+    image: "/assets/img/Small/Albañilería.webp",
+  },
+  {
+    id: "Electricidad",
+    title: "Electricidad",
+    image: "/assets/img/Small/Electricidad.webp",
+  },
+  {
+    id: "Pinturería",
+    title: "Pinturería",
+    image: "/assets/img/Small/Pinturería.webp",
+  },
+  {
+    id: "Plomería",
+    title: "Plomería",
+    image: "/assets/img/Small/Plomería.webp",
+  },
+  {
+    id: "Yesería",
+    title: "Yesería",
+    image: "/assets/img/Small/Yesería.webp",
+  },
+  {
+    id: "Durlock",
+    title: "Durlock",
+    image: "/assets/img/Small/Durlock.webp",
+  },
+];
