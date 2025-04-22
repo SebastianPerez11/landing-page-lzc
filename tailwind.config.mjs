23dc837afbe0b59e89ad4d6afd27361ad554c862
SebastianPerez11 <sebassperez11@gmail.com>
2025-04-22 10:51:06 -0300

    Add form and Obra section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-		gridTemplateColumns: {
-			'auto-services': 'repeat(auto-fit, minmax(450px, 1fr))',
-			'auto-services-mb': 'repeat(auto-fit, minmax(280px, 1fr))',
-			'auto-data': 'repeat(auto-fit, minmax(400px, 1fr))',
-			'auto-data-mb': 'repeat(auto-fit, minmax(280px, 1fr))',
-			'auto-12': 'repeat(12, minmax(0, 1fr))',
-		
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    gridTemplateColumns: {
+      "auto-services": "repeat(auto-fit, minmax(450px, 1fr))",
+      "auto-obras": "repeat(auto-fit, minmax(280px, 1fr))",
+      "auto-services-mb": "repeat(auto-fit, minmax(280px, 1fr))",
+      "auto-data": "repeat(auto-fit, minmax(400px, 1fr))",
+      "auto-data-mb": "repeat(auto-fit, minmax(280px, 1fr))",
+      "auto-12": "repeat(12, minmax(0, 1fr))",
+    },
+  },
+  plugins: [],
+};
